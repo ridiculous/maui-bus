083c9c2c376ee6c9404271302059950b11ab9191
Ryan Buckley <arebuckley@gmail.com>
2013-04-30 23:48:06 -1000

    update page titles

--- app/controllers/bus_routes_controller.rb
+++ app/controllers/bus_routes_controller.rb
@@ -5,6 +5,7 @@ class BusRoutesController < ApplicationController
   def show_map
     @region = Region.find(params[:region_name])
     @bus = @region.find_bus(params[:name])
+    @page_title = "Map for #{@bus.name} | Maui Bus"
     render layout: 'map'
   end
 end
