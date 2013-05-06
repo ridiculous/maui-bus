ee4ca24e41f1bded9aa4a337aec27f1305746cd0
Ryan Buckley <arebuckley@gmail.com>
2013-05-06 13:04:10 -1000

    add active class to upcoming stops button

--- app/assets/javascripts/regions.js
+++ app/assets/javascripts/regions.js
@@ -50,12 +50,17 @@ function showStaticMap() {
 }
 
 function ShowNextStops() {
-    var me = document.getElementById(this.rel);
+    var class_names = this.className.split(/\s+/)
+        , active_class = class_names.find('active')
+        , me = document.getElementById(this.rel);
 
     if (jUtils.hasClass(me, 'hidden-phone')) {
+        this.className += ' active';
         me.className = '';
     } else {
         me.className = 'hidden-phone hidden-tablet';
+        class_names.splice(active_class, 1);
+        this.className = class_names.join(' ');
     }
 }
 
