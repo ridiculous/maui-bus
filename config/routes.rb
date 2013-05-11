a93a1274025bc7b830669d32ebbd5bf53595d399
Ryan Buckley <arebuckley@gmail.com>
2013-05-10 22:22:59 -1000

    change route from bus/schedules to schedule ... its cleaner

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ MauiBusRoutes::Application.routes.draw do
   resources :sources, only: [:index]
   match 'regions/:name', to: 'regions#show', as: 'show_region'
   match 'regions/:region_name/routes/:name/map', to: 'bus_routes#show_map', as: 'show_bus_map'
-  match 'bus/schedules', to: 'regions#schedule', as: 'schedule'
+  match 'schedule', to: 'regions#schedule', as: 'schedule'
   root to: 'regions#index'
 
   # The priority is based upon order of creation:
