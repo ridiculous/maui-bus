0cfd4ef685e7da87525f92c4aa522e73bdb5cf7d
Ryan Buckley <arebuckley@gmail.com>
2013-05-11 14:06:13 -1000

    fix static map

--- app/assets/javascripts/static_map.js
+++ app/assets/javascripts/static_map.js
@@ -8,11 +8,12 @@ function StaticMap() {
         'zoom=16&size=530x400&maptype=roadmap&sensor=false&markers=color:green%7C';
 
     this.init = function () {
-        var coords = this.rel;
+        var coords = this.rel,
+            this_map_url = sm.map_url;
 
-        sm.map_url += coords;
-        sm.map_url += '&center=' + coords;  // center=20.886320,-156.475224
-        sm.$modal_body.innerHTML = '<img src="' + sm.map_url + '" width="530" height="400" />';
+        this_map_url += coords;
+        this_map_url += '&center=' + coords;  // center=20.886320,-156.475224
+        sm.$modal_body.innerHTML = '<img src="' + this_map_url + '" width="530" height="400" />';
         sm.$modal.className = 'modal';
         setTimeout(function () {
             jUtils.addEvent(document, 'keyup', sm.hideModalIf);
