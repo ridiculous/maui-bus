7acda9acfc21f4dd4956e1e2ea190ffa2e21dcd4
Ryan Buckley <arebuckley@gmail.com>
2013-06-18 19:51:58 -1000

    use deflator middleware and try ruby 2

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'https://rubygems.org'
 
-ruby '1.9.3'
+ruby '2.0.0'
 
-gem 'rails',     :git => 'git://github.com/rails/rails.git', :branch => '3-2-stable'
+gem 'rails',     '3.2.13'
 gem 'journey',   :git => 'git://github.com/rails/journey.git', :branch => '1-0-stable'
 gem 'arel',      :git => 'git://github.com/rails/arel.git', :branch => '3-0-stable'
 
--- lib/trip.rb
+++ lib/trip.rb
@@ -1,22 +1,26 @@
 class Trip
 
-  attr_accessor :origin, :destination, :starters, :finishers, :transfers
+  attr_accessor :origin, :destination, :transfers
 
-  # @param origin {Symbol|:queen_kaahumanu}
-  # @param destination {Symbol|:pukalani_terrace}
-  def initialize(origin='liholiho_kanaloa_ave', destination='alana_place_makawao')
-    @origin = origin.intern
-    @destination = destination.intern
-    @starters, @finishers, @transfers = [], [], []
+  def initialize(origin=:liholiho_kanaloa_ave, destination=:alana_place_makawao)
+    @origin = origin
+    @destination = destination
+    @transfers = []
   end
 
   # Loop through all routes and grab the ones that have our origin and destination in their list of stops
   def find_routes
     routes = Region.load_all.map(&:routes).flatten
-    routes.each do |rt|
-      locations = rt.stops.map(&:location)
-      starters << Run.new(rt, Location.transfers_only(locations)) if locations.include?(origin)
-      finishers << Run.new(rt, Location.transfers_only(locations)) if locations.include?(destination)
+    direct_routes = []
+    routes.each do |my_route|
+      locations = my_route.stops.map(&:location)
+      if locations.include?(origin)
+        
+        if locations.include?(destination)
+          direct_routes << my_route
+        end
+
+      end
     end
   end
 
@@ -25,29 +29,12 @@ class Trip
   # 2. Not on same route, but their routes have a similar transfer. Return routes and transfers
   # 3. Not on same route and no similar transfer. Search other routes for common transfer and return those
   def plot_course
-    if same_route?
-      self.journies = finishers.map{ |f| Journey.new(f) }
-    else
 
-    end
-  end
-
-  def same_route?
-    (starters.map(&:name) & finishers.map(&:name)).any?
   end
 
   class Run < Struct.new(:route, :transfers)
     def name
       route.name
-    end
-  end
-
-  class Journey
-    attr_reader :routes, :transfers
-
-    def initialize(routes, transfers=[])
-      @routes = routes
-      @transfers = transfers
     end
   end
 
