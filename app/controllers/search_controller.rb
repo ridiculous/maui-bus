47d860fe57755cdfa0a867cd5a6286bc9bdc7f6d
Ryan Buckley <arebuckley@gmail.com>
2013-08-18 13:50:01 -1000

    greatly improve search of direct routes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,10 @@
 class SearchController < ApplicationController
   layout 'map_hybrid'
+
   def index
     @trip = Trip.new(params[:origin], params[:destination])
-    @trips = @trip.find_direct_routes
+    @trip.find_direct_routes
+    @trips = @trip.prioritize.sort_by { |t| t.stop_at.time }
     @locations = Location.unique.map { |key, val| [val.to_s, key] }
   end
 end
--- lib/trip.rb
+++ lib/trip.rb
@@ -1,29 +1,33 @@
 class Trip
 
-  attr_accessor :origin, :destination, :transfers
+  attr_accessor :origin, :destination, :transfers, :direct_routes
 
   def initialize(origin=nil, destination=nil) #alana_place_makawao
     @origin = (origin || :liholiho_kanaloa_ave).to_sym
     @destination = (destination || :queen_kaahumanu).to_sym
     @transfers = []
+    @direct_routes = []
   end
 
   # Loop through all routes and grab the ones that have our origin and destination in their list of stops
-  def find_direct_routes
+  def find_direct_routes(current_time=Time.zone.now)
     routes = Region.load_all.map(&:routes).flatten
-    direct_routes = []
     routes.each do |my_route|
-      # all next stops
-      buses = my_route.next_stops(nil)
-      # cycle through each bus for this route
-      buses.each do |nxt_stop|
-        start_at = nxt_stop.find { |s| true_name(s.bus_stop.location) == true_name(origin) }
-        if start_at
-          new_buses = my_route.next_stops(nil, start_at.time)
-          new_buses.each do |origin_stops|
-            stop_at = origin_stops.find { |s| true_name(s.bus_stop.location) == true_name(destination) }
-            if stop_at && stop_at.time > start_at.time && !direct_routes.find { |d| d.name == my_route.name }
-              direct_routes << DirectRoute.new(my_route, start_at, stop_at)
+      # -nil- as first arg asks for all upcoming stops
+      buses = my_route.next_stops(nil, current_time)
+      # each bus for this route
+      buses.each do |nxt_stops|
+        # each stop for this bus
+        nxt_stops.each do |nxt|
+          # check for starting location
+          if true_name(nxt.bus_stop.location) == true_name(origin)
+            start_at = nxt
+            # upcoming stops w/ the start time as the cut off, so stopping point >= starting point
+            my_route.next_stops(nil, start_at.time).each do |origin_stops|
+              stop_at = origin_stops.find { |s| true_name(s.bus_stop.location) == true_name(destination) }
+              if stop_at
+                direct_routes << DirectRoute.new(my_route, start_at, stop_at)
+              end
             end
           end
         end
@@ -32,7 +36,12 @@ class Trip
     direct_routes
   end
 
-# helpers
+  # get most direct route for each bus
+  def prioritize
+    direct_routes.group_by(&:name).map { |name, my_direct_routes| my_direct_routes.sort.first }
+  end
+
+  # remove mauka, makai, across, etc from the location name
   def true_name(name)
     name.to_s.sub(Location::PARTNER_PATTERN, '')
   end
@@ -42,10 +51,36 @@ class Trip
 # 2. Not on same route, but their routes have a similar transfer. Return routes and transfers
 # 3. Not on same route and no similar transfer. Search other routes for common transfer and return those
 
-  class DirectRoute < Struct.new(:route, :start_at, :stop_at)
+  class DirectRoute
+
+    attr_reader :route, :start_at, :stop_at, :rank
+
+    def initialize(route, start_at, stop_at)
+      @route = route
+      @start_at = start_at
+      @stop_at = stop_at
+      @rank = stop_at.time - start_at.time
+    end
+
     def name
       route.name
     end
+
+    # called by Array#sort
+    def <=>(other)
+      rank - other.rank
+    end
+  end
+
+  #
+  # Pre-test tests
+  #
+
+  def self.test
+    noon = Time.zone.parse('Sun, 18 Aug 2013 12:00:00')
+    @trip = new('kahului_airport', 'queen_kaahumanu')
+    @trip.find_direct_routes(noon)
+    @trip
   end
 
 end
