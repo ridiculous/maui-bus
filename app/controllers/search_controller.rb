1f4c9555cda7cda6fcde287f1238d164abcfba0f
Ryan Buckley <arebuckley@gmail.com>
2013-08-19 23:39:34 -1000

    fix bug with multiple buses

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
 
   def index
     @trip = Trip.new(params[:origin], params[:destination])
-    @trip.find_direct_routes
+    @trip.find_direct_routes#(Time.zone.parse('Mon, 19 Aug 2013 12:27:40'))
     @trips = @trip.prioritize.sort_by { |t| t.stop_at.time }
     @locations = Location.unique.map { |key, val| [val.to_s, key] }
   end
--- lib/trip.rb
+++ lib/trip.rb
@@ -5,13 +5,14 @@
 
 class Trip
 
-  attr_accessor :origin, :destination, :transfers, :direct_routes
+  attr_accessor :origin, :destination, :transfers, :direct_routes, :indirect_routes
 
-  def initialize(origin=nil, destination=nil) #alana_place_makawao
+  def initialize(origin=nil, destination=nil) # alana_place_makawao
     @origin = origin || 'liholiho_kanaloa_ave'
     @destination = destination || 'queen_kaahumanu'
     @transfers = []
     @direct_routes = []
+    @indirect_routes = []
   end
 
   #
@@ -21,17 +22,20 @@ class Trip
   # Loop through all routes and grab the ones that have our origin and destination in their list of stops
   def find_direct_routes(current_time=Time.zone.now)
     Region.load_all.map(&:routes).flatten.each do |my_route|
-      my_route.next_stops(nil, current_time).each do |nxt_stops|
+      my_route.next_stops(nil, current_time).each_with_index do |nxt_stops, start_bus|
         # each stop for this bus
         nxt_stops.each do |nxt|
           # check for starting location
           if true_name(nxt.bus_stop.location) == origin
             # upcoming stops w/ the start time as the cut off, so stopping point >= starting point
-            my_route.next_stops(nil, nxt.time).each do |origin_stops|
+            my_route.next_stops(nil, nxt.time).each_with_index do |origin_stops, stop_bus|
               stop_at = origin_stops.find { |s| true_name(s.bus_stop.location) == destination }
-              if stop_at
+
+              # make sure this is the same bus
+              if stop_at && start_bus == stop_bus
                 direct_routes << DirectRoute.new(my_route, nxt, stop_at)
               end
+
             end
           end
         end
@@ -48,6 +52,27 @@ class Trip
   # remove mauka, makai, across, etc from the location name
   def true_name(name)
     name.to_s.sub(Location::PARTNER_PATTERN, '')
+  end
+
+  #
+  # Indirect Routes and Transfers
+  #
+
+  # find and save bus routes that include -origin- or -destination- in list of stops
+  # and then join them together on the their transfer points
+  def find_indirect_routes(current_time=Time.zone.now)
+    Region.load_all.map(&:routes).flatten.each do |my_route|
+      my_route.next_stops(nil, current_time).each do |nxt_stops|
+        # each stop for this bus
+        nxt_stops.each do |nxt|
+          # check for starting location
+          if true_name(nxt.bus_stop.location) == origin
+            indirect_routes << '...'
+          end
+        end
+      end
+    end
+    indirect_routes
   end
 
   #
