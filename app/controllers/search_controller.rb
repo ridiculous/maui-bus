97df6ca7bdb06959bace130d2bf1a266fd8b5b63
Ryan Buckley <arebuckley@gmail.com>
2013-08-21 17:18:18 -1000

    set header of ajax request ... the rails way

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
     @trip = Trip.new(params[:origin], params[:destination])
     @trip.find_direct_routes#(Time.zone.parse('Mon, 19 Aug 2013 12:27:40'))
     @trips = @trip.prioritize.sort_by { |t| t.stop_at.time }
-    @locations = Location.unique.map { |key, val| [val.to_s, key] } unless params[:async]
-    render partial: 'direct_routes' if params[:async]
+    @locations = Location.unique.map { |key, val| [val.to_s, key] } unless request.xhr?
+    render partial: 'direct_routes' if request.xhr?
   end
 end
