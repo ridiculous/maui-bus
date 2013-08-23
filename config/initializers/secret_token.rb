bec25ef502bc42bb0aa292050ba4d04c7f3b8217
Ryan Buckley <arebuckley@gmail.com>
2013-08-22 15:15:24 -1000

    fix spec and add secretkeybase

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 MauiBusRoutes::Application.config.secret_token = '60478a29bee6c40e46835ea6bcf1c2d5848cd9a9e8720c878481330b9c92d99054ff59f9e3336f46d466baf6e6ad821e3d99af6a0cd2287af38e5b0a85e0a55f'
+MauiBusRoutes::Application.config.secret_key_base = 'e10281f7e4295f5b6ac15497ac988db05db69d9680d4353a7aeacc6bc11d545b3c1b3a9d9808d1d4a8a1ba688d038f4e9a310a3405084c1808bd2c6eca778bef'
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -6,7 +6,9 @@ describe SearchController do
     it 'should render index page' do
       get :index
       response.should be_success
-      response.should have_rendered('layouts/map', 'search/index')
+      response.should have_rendered('layouts/map_hybrid', 'search/index')
+      assigns(:trip).should be_present
+      assigns(:trips).should be_present
     end
   end
 end
