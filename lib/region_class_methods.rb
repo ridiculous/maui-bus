02af6bb99985c7ee8479ad7a9045a127f58dff46
Ryan Buckley <arebuckley@gmail.com>
2013-08-25 14:37:18 -1000

    method moved to bus_route.rb

--- lib/region_class_methods.rb
+++ lib/region_class_methods.rb
@@ -10,9 +10,4 @@ module RegionClassMethods
     end
   end
 
-  # Lahaina.last_stop_time(:villager) #=> 23:00:00 -1000
-  def last_stop_time(name=:islander)
-    send(name).stops.map(&:times).flatten.reject(&:empty?).map(&:to_time).last
-  end
-
 end
